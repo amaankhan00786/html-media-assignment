9eb28787ec5c3fe0ac9a5a07924d09afa74a3a80
amaankhan00786 <128613301+amaankhan00786@users.noreply.github.com>
2023-12-15 16:06:06 +0530

    Add files via upload

---
+++ HOF Assignment/1. Reverse String/script.js
@@ -0,0 +1,13 @@
+
+// Reverse string 
+let name = "Rishabh";
+let bag = "";
+setTimeout(() => {
+     for (let i = name.length-1; i >= 0; i--) {
+      bag+=name[i]
+    }
+    console.log(bag);
+}, 2000);
+
+
+
